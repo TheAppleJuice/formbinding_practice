8a271a5c16c4194ff34c7be4eb0ca36e3df4e5eb
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-04-14 15:08:08 +0200

    Added CustomerDetails entity

--- src/main/java/se/lexicon/formbinding_practice/entity/CustomerDetails.java
+++ src/main/java/se/lexicon/formbinding_practice/entity/CustomerDetails.java
@@ -1,4 +1,22 @@
 package se.lexicon.formbinding_practice.entity;
 
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class CustomerDetails {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name= "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String detailsId;
+    private String street;
+    private String zipCode;
+    private String city;
+    private String homePhone;
+    private String cellPhone;
 }
