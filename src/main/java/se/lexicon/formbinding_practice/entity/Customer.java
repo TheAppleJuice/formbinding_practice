37b86b31d58223a8a83b59e8d40666873730d96d
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-04-14 15:04:57 +0200

    Add Customer entity

--- src/main/java/se/lexicon/formbinding_practice/entity/Customer.java
+++ src/main/java/se/lexicon/formbinding_practice/entity/Customer.java
@@ -1,4 +1,22 @@
 package se.lexicon.formbinding_practice.entity;
 
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import java.time.LocalDate;
+
+@Data
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String customerId;
+    private String email;
+    private LocalDate regDate;
+    private boolean active;
+    private CustomerDetails customerDetails;
 }
