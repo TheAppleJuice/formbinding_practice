2b55aaea03b79acac9d031b56cc964886ecd0245
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-04-15 13:36:54 +0200

    added CustomerConverter methods

--- src/main/java/se/lexicon/formbinding_practice/converter/CustomerConverter.java
+++ src/main/java/se/lexicon/formbinding_practice/converter/CustomerConverter.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import se.lexicon.formbinding_practice.dto.CustomerDto;
 import se.lexicon.formbinding_practice.entity.Customer;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 @Component
@@ -20,21 +21,50 @@ public class CustomerConverter implements Converter <Customer, CustomerDto> {
 
     @Override
     public Customer toModel(CustomerDto dto) {
-        return null;
+        Customer customer = new Customer();
+        if (dto!=null){
+            customer.setCustomerId(dto.getCustomerId());
+            customer.setEmail(dto.getEmail());
+            customer.setRegDate(dto.getRegDate());
+            customer.setActive(dto.isActive());
+            customer.setCustomerDetails(customerDetailsConverter.toModel(dto.getCustomerDetailsDto()));
+        }
+        return customer;
     }
 
     @Override
     public CustomerDto toDTO(Customer model) {
+        CustomerDto customerDto = new CustomerDto();
+        if (model != null){
+            customerDto.setCustomerId(model.getCustomerId());
+            customerDto.setEmail(model.getEmail());
+            customerDto.setRegDate(model.getRegDate());
+            customerDto.setActive(model.isActive());
+            customerDto.setCustomerDetailsDto(customerDetailsConverter.toDTO(model.getCustomerDetails()));
+        }
         return null;
     }
 
     @Override
     public Collection<Customer> toModels(Collection<CustomerDto> collection) {
-        return null;
+        Collection<Customer> customers = new ArrayList<>();
+        if (collection != null){
+            for (CustomerDto customerDto : collection){
+                customers.add(toModel(customerDto));
+            }
+        }
+        return customers;
     }
 
     @Override
     public Collection<CustomerDto> toDTos(Collection<Customer> collection) {
-        return null;
+        Collection<CustomerDto> customerDtos = new ArrayList<>();
+        if (collection != null){
+            for (Customer customer : collection){
+                CustomerDto dto = toDTO(customer);
+                customerDtos.add(dto);
+            }
+        }
+        return customerDtos;
     }
 }
