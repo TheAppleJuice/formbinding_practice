d1cccef74623215dbacb99dabd857e471248e264
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-04-15 12:39:59 +0200

    added repository functionality

---
+++ src/main/java/se/lexicon/formbinding_practice/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package se.lexicon.formbinding_practice.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import se.lexicon.formbinding_practice.entity.Customer;
+
+public interface CustomerRepository extends CrudRepository<Customer, String> {
+}
